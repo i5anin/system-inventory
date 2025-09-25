72dd54cf4126fbd0011161ce924ea5ffd3fd2a10
Isanin Sergey <sergey.isanin+github@gmail.com>
2025-09-25 23:48:29 +0300

    Update search.ts

--- docs/.vitepress/config/search.ts
+++ docs/.vitepress/config/search.ts
@@ -1,24 +1,22 @@
-// .vitepress/search.ts
+// .vitepress/config/search.ts
 export const search = {
-    provider: 'local',
-    options: {
-        locales: {
-            root: {
-                translations: {
-                    button: {
-                        buttonText: 'Поиск',
-                        buttonAriaLabel: 'Открыть поиск'
-                    },
-                    modal: {
-                        displayDetails: 'Показать подробности',
-                        resetButtonTitle: 'Очистить',
-                        backButtonTitle: 'Назад',
-                        noResultsText: 'Ничего не найдено',
-                        footer: {
-                            selectText: 'выбрать',
-                            navigateText: 'навигация',
-                            closeText: 'закрыть'
-                        }
+    provider: 'local' as const,
+    locales: {
+        root: {
+            translations: {
+                button: {
+                    buttonText: 'Поиск',
+                    buttonAriaLabel: 'Открыть поиск'
+                },
+                modal: {
+                    displayDetails: 'Показать подробности',
+                    resetButtonTitle: 'Очистить',
+                    backButtonTitle: 'Назад',
+                    noResultsText: 'Ничего не найдено',
+                    footer: {
+                        selectText: 'выбрать',
+                        navigateText: 'навигация',
+                        closeText: 'закрыть'
                     }
                 }
             }
